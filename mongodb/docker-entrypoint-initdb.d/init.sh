fa6cdbafc0942cc221922b61e4d004a9e272782e
YURII D <gkhelloworld@gmail.com>
2023-10-24 15:13:58 +0300

    clone

---
+++ mongodb/docker-entrypoint-initdb.d/init.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [[ -f "${MONGO_INIT_COMPLETE_FILE_PATH}" ]]; then
+  echo "MongoDB initiation has been completed."
+else
+   # Create a MongoDB user
+  mongosh -u "${MONGO_INITDB_ROOT_USERNAME}" -p "${MONGO_INITDB_ROOT_PASSWORD}" <<EOF
+    use ${APP_MONGO_DATABASE}
+    db.createUser({
+      user: "${APP_MONGO_USERNAME}",
+      pwd: "${APP_MONGO_PASSWORD}",
+      roles: [ {
+        role: "readWrite",
+        db: "${APP_MONGO_DATABASE}",
+      } ]
+    })
+    db.createCollection("initDB")
+    db.initDB.insertOne({
+        createdAt: new Date(),
+    })
+EOF
+  # Timeout
+  sleep 5
+  # Create file
+  touch "${MONGO_INIT_COMPLETE_FILE_PATH}"
+  echo "The creation of the MongoDB user and database is complete."
+fi
